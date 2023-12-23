88dde4ac5adea15d627ea77398c7f532aac14b99
Buntin-Liz <mail@synthia-liz.com>
2023-12-24 04:04:35 +0900

    a

--- src/MailNotification/index.ts
+++ src/MailNotification/index.ts
@@ -1,27 +1,29 @@
-import fs from 'fs';
-import path from 'path';
-import { Channel, Events, SlashCommandBuilder } from 'discord.js';
+// import fs from 'fs';
+// import path from 'path';
+// import { Channel, Events, SlashCommandBuilder } from 'discord.js';
 
-import { Division } from '../Division';
-import { Core } from '../Core';
-import * as util from '../utils';
+// import { Division } from '../Division';
+// import { Core } from '../Core';
+// import * as util from '../utils';
 
-export class MailNotification extends Division {
-  /* 
-  p::name:string
-  ronly::core:Core
-  p::commands:Command[]
-  p::eventSets:EventSet[]
-  p:: division_data_dir: string
-  */
-  private online: boolean;
-  private channels: Channel[];
-  constructor(core: Core) {
-    super(core);
-    if () {
-    } else {
-    }
+// export class MailNotification extends Division {
+//   /* 
+//   p::name:string
+//   ronly::core:Core
+//   p::commands:Command[]
+//   p::eventSets:EventSet[]
+//   p:: division_data_dir: string
+//   */
+//   private online: boolean;
+//   private channels: Channel[];
+//   constructor(core: Core) {
+//     super(core);
+//     this.online = false;
+//     this.channels = [];
+//     if () {
+//     } else {
+//     }
 
-    this.printInitMessage();
-  }
-}
+//     this.printInitMessage();
+//   }
+// }
